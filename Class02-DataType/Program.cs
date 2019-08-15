13b28ae96a62c09ca5f39961e8cdb731b08cc127
Fowindy <fowindy@outlook.com>
2019-08-15 15:05:38 +0800

    字面值(默认值)(整数->int;小数->double)

--- Class02-DataType/Program.cs
+++ Class02-DataType/Program.cs
@@ -10,9 +10,9 @@ namespace Class02_DataType
     {
         static void Main(string[] args)
         {
-            //double类型(15~16位有效数字)
-            double num = 12312323.123456789123456789;
-            Console.WriteLine(num);//12312323.1234568   //15位有效数字
+            //字面值(默认值)(整数->int;小数->double)
+            //double num = 12312323.123456789123456789;
+            Console.WriteLine(12312323.123456789123456789);//12312323.1234568   //15位有效数字
             Console.Read();//暂停控制台
 
             //金融数据decimal类型(涉及到金钱全部使用decimal)(28~29位有效数字)
