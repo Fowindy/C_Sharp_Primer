a8692b6f660ec338f991905fec84b6434b6c44c1
Fowindy <fowindy@outlook.com>
2019-08-15 20:49:21 +0800

    二元运算符:a++与++a的区别(++a:++在前;先自增后赋值)

--- Class03-OperatorAndBranches/Program.cs
+++ Class03-OperatorAndBranches/Program.cs
@@ -12,11 +12,11 @@ namespace Class03_OperatorAndBranches
         {
             //一元运算符:+(正号);-(负号);
             //二元运算符:+(加);-(减);*(乘);/(除);%(取余);=(赋值);++(自增);--(自减);+=;-=;*=;/=;%=;
-            //二元运算符:a++与++a的区别(a++:++在后;先赋值再自增)
+            //二元运算符:a++与++a的区别(++a:++在前;先自增后赋值)
             int a = 10;
             int b;
-            b = a++;//++在后;先赋值再自增
-            Console.WriteLine(b);//10
+            b = ++a;//++在前;先自增后赋值
+            Console.WriteLine(b);//11
             Console.WriteLine(a);//11
             Console.Read();
         }
