a506358efac9246661e2256ed6f319c6d9e04c8b
Fowindy <fowindy@outlook.com>
2019-08-15 15:08:24 +0800

    字面值(指数形式E代表以10为底)

--- Class02-DataType/Program.cs
+++ Class02-DataType/Program.cs
@@ -13,9 +13,14 @@ namespace Class02_DataType
             //字面值(默认值)(整数->int;小数->double)
             //double num = 12312323.123456789123456789;
             Console.WriteLine(12312323.123456789123456789);//12312323.1234568   //15位有效数字
-            Console.Read();//暂停控制台
+            //Console.Read();//暂停控制台
 
             //金融数据decimal类型(涉及到金钱全部使用decimal)(28~29位有效数字)
+
+            //字面值(指数形式E代表以10为底)
+            double a = 2E10;//20000000000   //2*10^10
+            Console.WriteLine(a);
+            Console.Read();
         }
     }
 }
