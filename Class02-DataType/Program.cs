d80e7acd7c0f83a8f65110b7d8f5de90ea5a21cb
Fowindy <fowindy@outlook.com>
2019-08-15 15:00:09 +0800

    double类型有效数字15位测试1

--- Class02-DataType/Program.cs
+++ Class02-DataType/Program.cs
@@ -10,6 +10,10 @@ namespace Class02_DataType
     {
         static void Main(string[] args)
         {
+            //double类型(15~16位有效数字)
+            double num = 1.123456789123456789;
+            Console.WriteLine(num);//1.12345678912346   //15位有效数字
+            Console.Read();//暂停控制台
         }
     }
 }
