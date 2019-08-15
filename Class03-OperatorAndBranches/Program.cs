10a5f23eaf1680053b40cae06bf62b9579227501
Fowindy <fowindy@outlook.com>
2019-08-15 20:39:14 +0800

    二元运算符(%:取余)

---
+++ Class03-OperatorAndBranches/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class03_OperatorAndBranches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //一元运算符:+(正号);-(负号);
+            //二元运算符:+(加);-(减);*(乘);/(除);%(取余);=(赋值);++(自增);--(自减);+=;-=;*=;/=;%=;
+            int a = 10 % 3;
+            Console.WriteLine(a);//1
+            Console.Read();
+        }
+    }
+}
