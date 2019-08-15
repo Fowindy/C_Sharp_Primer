7865378055c44f3899d887a13a5f4bf319b2ebc9
Fowindy <fowindy@outlook.com>
2019-08-16 05:44:02 +0800

    编程的三大结构_分支:(else只跟最近的if:嵌套简写---多分支)

--- Class03-OperatorAndBranches/Program.cs
+++ Class03-OperatorAndBranches/Program.cs
@@ -11,19 +11,14 @@ namespace Class03_OperatorAndBranches
         static void Main(string[] args)
         {
             //编程的三大结构:顺序,分支,循环
-            //编程的三大结构_分支:(if后面只有一条语句,则可以省略大括号简写)
+            //编程的三大结构_分支:(else只跟最近的if:嵌套简写---多分支)
             int a = int.Parse(Console.ReadLine());
             if (a > 10)
                 Console.WriteLine("a大于10");
+            else if (a == 10)
+                Console.WriteLine("a等于10");
             else
-            {
-                #region 嵌套if
-                if (a == 10)
-                    Console.WriteLine("a等于10");
-                else
-                    Console.WriteLine("a小于10");
-                #endregion
-            }
+                Console.WriteLine("a小于10");
             Console.Read();
         }
     }
