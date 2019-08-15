c2932357a11ed05613063a72ed22752a1cc93d4b
Fowindy <fowindy@outlook.com>
2019-08-15 15:03:19 +0800

    金融数据decimal类型(涉及到金钱全部使用decimal)(28~29位有效数字)

--- Class02-DataType/Program.cs
+++ Class02-DataType/Program.cs
@@ -14,6 +14,8 @@ namespace Class02_DataType
             double num = 12312323.123456789123456789;
             Console.WriteLine(num);//12312323.1234568   //15位有效数字
             Console.Read();//暂停控制台
+
+            //金融数据decimal类型(涉及到金钱全部使用decimal)(28~29位有效数字)
         }
     }
 }
