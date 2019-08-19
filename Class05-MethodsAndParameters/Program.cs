5c8d3c1052488ca700c29f138b60e7336aed543c
Fowindy <fowindy@outlook.com>
2019-08-19 15:09:10 +0800

    方法是什么?

---
+++ Class05-MethodsAndParameters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class05_MethodsAndParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 方法是什么?
+             * 首先看看方法涉及的基本元素:
+             * System.Console.Write("name");
+             * 命名空间  类     方法  参数
+             */
+        }
+    }
+}
