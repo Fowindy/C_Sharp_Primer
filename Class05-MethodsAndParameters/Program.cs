bc4936f31b06034cf3c0057f7e77bfc465bf0a03
Fowindy <fowindy@outlook.com>
2019-08-19 16:40:16 +0800

    值传递:相当于复制过来的,是一份拷贝,外面和里面不搭噶;值传递的本质是复制;

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -26,6 +26,13 @@ namespace Class05_MethodsAndParameters
             }
             return (double)sum / nums.Count;
         }
+
+        //传值传引用的讲解:
+        static void Swap(int num1,int num2)
+        {
+            int temp;
+            temp = num1;num1 = num2;num2 = temp;
+        }
         static void Main(string[] args)
         {
             var list = new List<int>();
@@ -37,6 +44,11 @@ namespace Class05_MethodsAndParameters
             //调用方法参数的名称和方法命名的参数的名称可以不一致
             //实际参数
             Console.WriteLine("list列表的平均值为:{0}",Average(list));//2.75
+
+            int a = 1;
+            int b = 2;
+            Swap(a, b);//值传递:相当于复制过来的,是一份拷贝,外面和里面不搭噶;值传递的本质是复制;
+            Console.WriteLine("a={0},b={1}", a, b);
             Console.Read();
         }
     }
