4ca01c76c8b1affa08974addfba6a17215340260
Fowindy <fowindy@outlook.com>
2019-08-19 20:31:20 +0800

    例子8-1:继续修改,加入计算文件或计算目录的开关_默认参数_验证OK

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -10,11 +10,11 @@ namespace Class05_MethodsAndParameters
 {
     class Program
     {
-        //例子8:继续修改,加入计算文件或计算目录的开关_验证OK
+        //例子8-1:继续修改,加入计算文件或计算目录的开关_默认参数_验证OK
         /*
          * 理解 file和directory
          */
-        static long FileOrDirCount(string path,bool onlyFiles,bool onlyDir)
+        static long FileOrDirCount(string path,bool onlyFiles,bool onlyDir,String extension = ".txt")//默认参数
         {
             long count = 0;
             //捕获异常:try{}catch(){}finally{}
@@ -24,7 +24,20 @@ namespace Class05_MethodsAndParameters
                 {
                     //统计文件file的个数:
                     var files = Directory.GetFiles(path);
-                    count += files.Length;
+                    if (!string.IsNullOrEmpty(extension))
+                    {
+                        foreach (var file in files)
+                        {
+                            if (file.ToLower().EndsWith(extension))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        count += files.Length;
+                    }
                 }
                 var dirs = Directory.GetDirectories(path);
                 if (!onlyFiles)
@@ -34,7 +47,7 @@ namespace Class05_MethodsAndParameters
                 }
                 foreach (var dir in dirs)
                 {
-                    count += FileOrDirCount(dir, onlyFiles,onlyDir);
+                    count += FileOrDirCount(dir, onlyFiles,onlyDir, extension);
                 }
             }
             catch (DirectoryNotFoundException ex)
@@ -52,7 +65,7 @@ namespace Class05_MethodsAndParameters
         static void Main(string[] args)
         {
             Console.WriteLine("正在统计中...");
-            Console.WriteLine("该目录文件总数为:{0};文件夹的个数为:{1}", FileOrDirCount(@"J:\Documents\学习笔记",true,false), FileOrDirCount(@"J:\Documents\学习笔记",false,true));
+            Console.WriteLine("该目录文件总数为:{0};文件夹的个数为:{1};", FileOrDirCount(@"J:\Documents\学习笔记",true,false), FileOrDirCount(@"J:\Documents\学习笔记",false,true));
             Console.Read();
         }
     }
