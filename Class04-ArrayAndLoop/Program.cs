cfa55cfa48ee5b68988cbc8df948e525b80757aa
Fowindy <fowindy@outlook.com>
2019-08-19 11:39:25 +0800

    为什么使用数组(举例:斐波那契数列)

---
+++ Class04-ArrayAndLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class04_ArrayAndLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 为什么使用数组(举例:斐波那契数列)
+            /*
+            * 为什么使用数组?
+            * 
+            * 举个例子:斐波那契数列
+            * 1 1 2 3 5 8 13......
+            */
+            #endregion
+        }
+    }
+}
