255c00e1a5971d03776b5cb6cf77cbe9dc1c5bb8
Fowindy <fowindy@outlook.com>
2019-08-19 09:59:57 +0800

    编程的三大结构_分支:Switch

--- Class03-OperatorAndBranches/Program.cs
+++ Class03-OperatorAndBranches/Program.cs
@@ -11,14 +11,22 @@ namespace Class03_OperatorAndBranches
         static void Main(string[] args)
         {
             //编程的三大结构:顺序,分支,循环
-            //编程的三大结构_分支:(else只跟最近的if:嵌套简写---多分支)
+            //编程的三大结构_分支:Switch
             int a = int.Parse(Console.ReadLine());
-            if (a > 10)
-                Console.WriteLine("a大于10");
-            else if (a == 10)
-                Console.WriteLine("a等于10");
-            else
-                Console.WriteLine("a小于10");
+            switch (a)
+            {
+                case 1:Console.WriteLine("数字1");break;
+                case 2:Console.WriteLine("数字2");break;
+                default://默认,其他情况
+                    Console.WriteLine("数字未知");break;
+            }
+            /*
+             * 注意:
+             * default可以放在任何位置;
+             * 如果case1和case2执行的代码相同,则可以这样写:
+             * case1:case2:Console.WriteLine("数字1");break;
+             * 每种执行的情况必须加break
+             */
             Console.Read();
         }
     }
