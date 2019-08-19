fe7b14bbb9f9b57ff34e535a3bd3538e76aed8bc
Fowindy <fowindy@outlook.com>
2019-08-19 16:11:22 +0800

    方法的规范命名(每个单词的首字母大写)

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -11,13 +11,14 @@ namespace Class05_MethodsAndParameters
     {
         //例子1:写一个什么都不做的函数(方法),方法是在类里面
         //无static为动态方法
-        static void Test() { }
+        //方法的规范命名(每个单词的首字母大写)
+        static void ThisIsATest() { }
         //有static为静态方法,无static叫动态方法;静态方法无法调用动态方法
         //动态方法既可以调用动态方法也可以调用静态方法;静态方法只能调用静态方法;
         static void Main(string[] args)
         {
             //静态方法可以调用静态方法
-            Test();
+            ThisIsATest();
             /*
              * 方法是什么?
              * 首先看看方法涉及的基本元素:
@@ -33,6 +34,7 @@ namespace Class05_MethodsAndParameters
 
             //快捷键:ctrl + tab 切换工程
             //快捷键:alt + tab 切换任务
+
             Class1.Func();
 
             Console.Read();
