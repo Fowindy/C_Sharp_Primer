0b371e9cfe3a0f80e95424be76938980ac13ce3b
Fowindy <fowindy@outlook.com>
2019-08-19 10:18:18 +0800

    布尔表达式:求值结果为false或true的表达式

--- Class03-OperatorAndBranches/Program.cs
+++ Class03-OperatorAndBranches/Program.cs
@@ -10,23 +10,8 @@ namespace Class03_OperatorAndBranches
     {
         static void Main(string[] args)
         {
-            //编程的三大结构:顺序,分支,循环
-            //编程的三大结构_分支:Switch(开关可以是数值类型,也可以是字符类型)
-            string a = Console.ReadLine();
-            switch (a)//开关可以是数值类型,也可以是字符类型
-            {
-                case "1":Console.WriteLine("数字1");break;
-                case "a":Console.WriteLine("字母a");break;
-                default://默认,其他情况
-                    Console.WriteLine("字符未知");break;
-            }
-            /*
-             * 注意:
-             * default可以放在任何位置;
-             * 如果case1和case2执行的代码相同,则可以这样写:
-             * case1:case2:Console.WriteLine("数字1");break;
-             * 每种执行的情况必须加break
-             */
+            //布尔表达式:求值结果为false或true的表达式
+            bool exp = 2 <= 2;//true(2等于2或2小于2)
             Console.Read();
         }
     }
