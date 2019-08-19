f7f6cb026db3e49dce2b7b1a9df7525150bec84c
Fowindy <fowindy@outlook.com>
2019-08-19 16:30:52 +0800

    调用方法参数的名称和方法命名的参数的名称可以不一致

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -16,14 +16,15 @@ namespace Class05_MethodsAndParameters
          * $传值,传引用ref,out参数
          */
 
-        static double Average(List<int> list)//传一个list类型的列表进来
+        //形式参数
+        static double Average(List<int> nums)//传一个list类型的列表进来
         {
             int sum = 0;
-            foreach (var item in list)
+            foreach (var item in nums)
             {
                 sum += item;
             }
-            return (double)sum / list.Count;
+            return (double)sum / nums.Count;
         }
         static void Main(string[] args)
         {
@@ -33,6 +34,8 @@ namespace Class05_MethodsAndParameters
             list.Add(3);
             list.Add(5);
 
+            //调用方法参数的名称和方法命名的参数的名称可以不一致
+            //实际参数
             Console.WriteLine("list列表的平均值为:{0}",Average(list));//2.75
             Console.Read();
         }
