ac93e8c2bc797c60880b241085510e0f74e9ed7a
Fowindy <fowindy@outlook.com>
2019-08-19 19:53:16 +0800

    例子7-2:统计目标目录下文件总数_递归实现_异常处理_文件计算成功_文件夹计算成功

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -10,25 +10,27 @@ namespace Class05_MethodsAndParameters
 {
     class Program
     {
-        //例子7-2:统计目标目录下文件总数_递归实现_异常处理_文件计算成功
+        //例子7-2:统计目标目录下文件总数_递归实现_异常处理_文件计算成功_文件夹计算成功
         /*
          * 理解 file和directory
          */
+        public static long countDires = 0;
 
-        static long FileOrDirCount(string path)
+        static long FileOrDirCount(string path,out long dires)
         {
-            long count = 0;
+            long countFiles = 0;
             //捕获异常:try{}catch(){}finally{}
             try
             {
                 //统计文件file的个数:
                 var files = Directory.GetFiles(path);
-                count += files.Length;
+                countFiles += files.Length;
                 //统计子目录directory的个数
                 var dirs = Directory.GetDirectories(path);
+                countDires += dirs.Length;
                 foreach (var dir in dirs)
                 {
-                    count += FileOrDirCount(dir);
+                    countFiles += FileOrDirCount(dir,out countDires);
                 }
             }
             catch (DirectoryNotFoundException ex)
@@ -41,12 +43,14 @@ namespace Class05_MethodsAndParameters
                 //打印异常
                 Console.WriteLine(ex2.Message);
             }
-            return count;
+            dires = countDires;
+            return countFiles;
         }
         static void Main(string[] args)
         {
+            long countDires;
             Console.WriteLine("正在统计中...");
-            Console.WriteLine("该目录文件总数为:{0}", FileOrDirCount(@"J:\Documents\学习笔记"));
+            Console.WriteLine("该目录文件总数为:{0};文件夹的个数为:{1}", FileOrDirCount(@"J:\Documents\学习笔记",out countDires), countDires);
             Console.Read();
         }
     }
