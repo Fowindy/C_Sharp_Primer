a861f1cd29fc5578134b305a95422cb57b5fbca4
Fowindy <fowindy@outlook.com>
2019-08-19 17:55:54 +0800

    例子6:累加案例_参数数量可变

--- Class05-MethodsAndParameters/Program.cs
+++ Class05-MethodsAndParameters/Program.cs
@@ -9,26 +9,18 @@ namespace Class05_MethodsAndParameters
 {
     class Program
     {
-        //例子4:继续交换案例_重载:函数名可以相同,但是参数不能完全一样
-        //例子5:还是交换案例_泛型:一种函数处理多种类型的功能
-        static void Swap<T>(ref T a,ref T b)
+        //例子6:累加案例_参数数量可变
+        /*
+         * $理解:参数数量可变参数也可以加入模板
+         */
+        static int Sum(params int[] nums)
         {
-            T temp;
-            temp = a;a = b;b = temp;
+            return nums.Sum();
         }
-
         static void Main(string[] args)
         {
-            int a = 1;
-            int b = 2;
-            Swap(ref a, ref b);//整型可以交换//完整写法Swap<int>(ref a, ref b);
-            Console.WriteLine("a={0},b={1}",a,b);//a=2,b=1
-
-            string str1 = "123";
-            string str2 = "abc";
-            Swap(ref str1, ref str2);//字符类型也可以完成交换//完整写法Swap<string>(ref str1, ref str2);
-            Console.WriteLine("str1={0},str2={1}", str1, str2);//str1=abc,str2=123
-
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6));//21
+            Console.WriteLine(Sum(1, 2, 3, 4, 5, 6,7,8,9));//45
             Console.Read();
         }
     }
