107efd6194deecbca45234eb7d6f3c0fba4e6479
Fowindy <fowindy@outlook.com>
2019-08-19 12:03:40 +0800

    为什么使用数组(举例:斐波那契数列_使用数组循环实现)

--- Class04-ArrayAndLoop/Program.cs
+++ Class04-ArrayAndLoop/Program.cs
@@ -10,21 +10,32 @@ namespace Class04_ArrayAndLoop
     {
         static void Main(string[] args)
         {
-            #region 为什么使用数组(举例:斐波那契数列_不使用数组实现_很繁琐)
+            #region 为什么使用数组(举例:斐波那契数列_使用数组循环实现)
             /*
             * 为什么使用数组?
             * 
             * 举个例子:斐波那契数列
             * 1 1 2 3 5 8 13......
             */
+            int[] a = new int[11];
+            a[1] = 1;
+            a[2] = 1;
 
-            int a1 = 1;
-            int a2 = 1;
+            int i = 3;
+            while (i != 11)
+            {
+                a[i] = a[i - 1] + a[i - 2];
+                i++;
+            }
 
-            int a3 = a2 + a1;
-            int a4 = a3 + a2;
-            int a5 = a4 + a3;
-            
+            int j = 1;
+            while (j != 11)
+            {
+                Console.WriteLine(a[j]);
+                j++;
+            }
+
+            Console.Read();
             #endregion
         }
     }
